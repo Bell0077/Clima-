f91fb81f9e500624889b7bf001e186ae1caac81c
Gabriel <gabrielfreire00077@gmail.com>
2025-10-21 22:54:49 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Weather from './Weather.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <h1>CLIMA</h1>
+      <Weather />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Weather.jsx
@@ -0,0 +1,62 @@
+import React,{ useEffect, useState } from "react";
+import axios from "axios";
+import './App.css'
+
+  const Weather = () => {
+    const [city, setCity ] = useState ('');
+    const [weatherData, setWeatherData] = useState (null);
+
+    const fetchData = async () => {
+     try{
+      const response = await axios.get (
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&units=metric&appid=21abb1b5d1481c715dd8d76696b94035`
+      );
+      setWeatherData (response.data);
+      console.log (response.data); 
+     } catch (error){
+      console.error(error);
+     }
+    };
+
+    useEffect(() => {
+      fetchData();
+    }, []);
+
+    const handleInputChange = (e) => {
+      setCity(e.target.value);
+    };
+
+    const haandleSubmit = (e) => {
+      e.preventDefault();
+      fetchData();
+    };
+
+    return (
+      <div className="home">
+        <form className= "clima" onSubmit={haandleSubmit}>
+          <input type="text"
+          placeholder="nome da cidade"
+          value={city}
+          onChange={handleInputChange}/>
+          <button type="submit">
+            procurar
+          </button>
+        </form>
+        {weatherData ? (
+           <>
+           <h2>{weatherData.name}</h2>
+           <p>Temperatura: {weatherData.main.temp}°C</p>
+           <p>Descrição: {weatherData.weather[0].description}</p>
+           <p>Sensação termica : {weatherData.main.feels_like}°C</p>
+           <p>Humidade do ar : {weatherData.main.humidity}%</p>
+           <p>Pressão: {weatherData.main.pressure}</p>
+           <p>velocidade dos ventos : {weatherData.wind.speed}m/s</p>
+         </>
+        ) : ( 
+          <p></p>
+        )}
+      </div>
+     );
+    };
+    
+    export default Weather; 
